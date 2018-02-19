953f97086951203480f45034ad334d3f1562d106
kosc <hotkosc@gmail.com>
2018-02-19 13:00:25 +0300

    Update app.py
    Absolute path to templates

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ DATABASES = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
